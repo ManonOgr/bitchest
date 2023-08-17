b87b19d0dd6a6daee369735d1bc3216d87261c90
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2023-08-17 15:36:24 +0200

    create page login

---
+++ client/src/components/AdminForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid fill-height>
+    <v-row justify="center" align-content="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card>
+          <v-card-title class="justify-center text-center">
+            <img src="@/assets/bitchest_logo.png" alt="Logo" height="100">
+          </v-card-title>
+          <v-card-text>
+            <v-form @submit.prevent="submitForm">
+              <v-text-field
+                v-model="email"
+                label="Email"
+                type="email"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-mode.l="password"
+                label="Password"
+                type="password"
+                required
+              ></v-text-field>
+              <v-btn color="primary" type="submit" class="text-center">Submit</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AdminForm from "@/components/AdminForm.vue";
+</script>
+
+<template>
+    <v-container fluid fill-height>
+      <v-row justify="center">
+        <v-col cols="12" sm="8" md="6">
+          <AdminForm />
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>

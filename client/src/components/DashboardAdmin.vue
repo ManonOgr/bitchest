352a6113ba641ac4426825c2f72c723798783a56
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2023-08-22 09:45:37 +0200

    retrieving data from the admin logged in

--- client/src/components/DashboardAdmin.vue
+++ client/src/components/DashboardAdmin.vue
@@ -8,6 +8,7 @@
             <p><strong>Nom :</strong> {{ nom }}</p>
             <p><strong>Prénom :</strong> {{ prenom }}</p>
             <p><strong>E-mail :</strong> {{ email }}</p>
+            <p><strong>Status :</strong> {{ status }}</p>
           </v-card-text>
         </v-card>
       </v-container>
@@ -16,16 +17,50 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
-      nom: 'Doe',
-      prenom: 'John',
-      email: 'john.doe@example.com'
+      nom: '',
+      prenom: '',
+      email: ''
     };
-  }
+  },
+  mounted() {
+    try {
+      axios
+        .post(
+          "http://127.0.0.1:8000/api/login",
+          {
+            email: 'admin@admin.com',
+              password: 'Admin',
+          },
+          {
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+            },
+          }
+        )
+        .then((res) => {
+          const userData = res.data.user; // Récupérer les données utilisateur depuis la réponse
+          
+          this.nom = userData.last_name;
+          this.prenom = userData.first_name;
+          this.email = userData.email;
+          this.status = userData.status;
+        });
+
+      // En cas d'erreur de connexion
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 </script>
 
 <style>
+/* Vos styles ici */
 </style>

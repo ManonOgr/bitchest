54dcc2c7fb172d2af41bf3804590051778767d8b
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2023-08-22 11:10:33 +0200

    display customer personal data

--- client/src/components/DashboardClient.vue
+++ client/src/components/DashboardClient.vue
@@ -3,11 +3,12 @@
     <v-main>
       <v-container>
         <v-card>
-          <v-card-title>Informations Client</v-card-title>
+          <v-card-title>Informations Admin</v-card-title>
           <v-card-text>
             <p><strong>Nom :</strong> {{ nom }}</p>
             <p><strong>Prénom :</strong> {{ prenom }}</p>
             <p><strong>E-mail :</strong> {{ email }}</p>
+            <p><strong>Status :</strong> {{ status }}</p>
           </v-card-text>
         </v-card>
       </v-container>
@@ -16,16 +17,50 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
-      nom: 'Quentin',
-      prenom: 'Leroy',
-      email: 'ekrrfrefe@example.com'
+      nom: '',
+      prenom: '',
+      email: ''
     };
-  }
+  },
+  mounted() {
+    try {
+      axios
+        .post(
+          "http://127.0.0.1:8000/api/login",
+          {
+            email: 'quentinleroy@example.com',
+              password: 'QuentinL',
+          },
+          {
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+            },
+          }
+        )
+        .then((res) => {
+          const userData = res.data.user; // Récupérer les données utilisateur depuis la réponse
+          
+          this.nom = userData.last_name;
+          this.prenom = userData.first_name;
+          this.email = userData.email;
+          this.status = userData.status;
+        });
+
+      // En cas d'erreur de connexion
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 </script>
 
 <style>
+/* Vos styles ici */
 </style>

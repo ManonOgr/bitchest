742d3e00b47796ceb27dc2014db65efa1918ff12
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2023-08-23 14:44:30 +0200

    walletController

--- api/app/Http/Controllers/WalletController.php
+++ api/app/Http/Controllers/WalletController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Wallet;
 use Illuminate\Http\Request;
 
 class WalletController extends Controller
 {
-    //
+    public function getWallet()
+    {
+        $currencies = Wallet::select('id', 'user_id', 'quantity')->get();
+
+        return response()->json($currencies);
+    }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CurrencyController;
 use App\Http\Controllers\HistoryController;
+use App\Http\Controllers\WalletController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,6 +38,7 @@ Route::delete('/users/{id}', [UserController::class, 'destroy']);
 Route::get('/currencies', [CurrencyController::class, 'getCurrencies']);
 
 Route::get('/history', [HistoryController::class, 'getHistory']);
+Route::get('/wallet', [WalletController::class, 'getController']);
 
 Route::get('/currencies/names', [CurrencyController::class, 'index']);
 

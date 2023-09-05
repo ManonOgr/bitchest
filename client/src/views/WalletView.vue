ff6c57b99cd2b2dbba62d07c74fb8febd039f029
ManonOgr <72557754+ManonOgr@users.noreply.github.com>
2023-09-05 11:18:21 +0200

    format date fr

--- client/src/views/WalletView.vue
+++ client/src/views/WalletView.vue
@@ -23,7 +23,8 @@
                 <th class="text-left">Date d'achat</th>
                 <th class="text-left">Prix d'achat</th>
                 <th class="text-left">Prix de vente</th>
-                <th class="text-left">Plus Value</th> <!-- New column for Plus Value -->
+                <th class="text-left">Plus Value</th>
+                <!-- New column for Plus Value -->
                 <th class="text-left">Actions</th>
               </tr>
             </thead>
@@ -32,16 +33,36 @@
                 <td class="text-left">{{ transaction.currency_id }}</td>
                 <td class="text-left">{{ transaction.currency_name }}</td>
                 <td class="text-left">{{ transaction.quantity }}</td>
-                <td class="text-left">{{ transaction.purchase_date }}</td>
+                <td class="text-left">
+                  {{ formatPurchaseDate(transaction.purchase_date) }}
+                </td>
                 <td class="text-left">{{ transaction.purchase_price }}</td>
                 <td class="text-left">{{ transaction.selling_price }}</td>
-                <td class="text-left" :class="{ 'text-red': isNegativeCapitalGain(transaction), 'text-green': isPositiveCapitalGain(transaction) }">
+                <td
+                  class="text-left"
+                  :class="{
+                    'text-red': isNegativeCapitalGain(transaction),
+                    'text-green': isPositiveCapitalGain(transaction),
+                  }"
+                >
                   {{ formattedCapitalGain(transaction) }}
-                  <span v-if="isNegativeCapitalGain(transaction)" class="arrow-down">▼</span>
-                  <span v-if="isPositiveCapitalGain(transaction)" class="arrow-up">▲</span>
+                  <span
+                    v-if="isNegativeCapitalGain(transaction)"
+                    class="arrow-down"
+                    >▼</span
+                  >
+                  <span
+                    v-if="isPositiveCapitalGain(transaction)"
+                    class="arrow-up"
+                    >▲</span
+                  >
                 </td>
                 <td class="text-left">
-                  <v-btn class="btn" color="primary" @click="confirmSellCrypto(transaction)">
+                  <v-btn
+                    class="btn"
+                    color="primary"
+                    @click="confirmSellCrypto(transaction)"
+                  >
                     Vendre
                   </v-btn>
                 </td>
@@ -60,7 +81,9 @@
           Êtes-vous sûr de vouloir vendre cette crypto ?
         </v-card-text>
         <v-card-actions>
-          <v-btn color="primary" @click="sellCrypto(transactionToSell)">Confirmer</v-btn>
+          <v-btn color="primary" @click="sellCrypto(transactionToSell)"
+            >Confirmer</v-btn
+          >
           <v-btn color="error" @click="cancelSellCrypto">Annuler</v-btn>
         </v-card-actions>
       </v-card>
@@ -125,6 +148,13 @@ export default {
     }
   },
   methods: {
+    formatPurchaseDate(dateString) {
+      if (!dateString) return "";
+
+      const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+      const date = new Date(dateString);
+      return date.toLocaleDateString("fr-FR", options);
+    },
     async calculateTotalBalanceEuros() {
       let totalBalanceEuros = 0;
       for (const transaction of this.userTransactions) {
@@ -144,13 +174,18 @@ export default {
     async sellCrypto(transaction) {
       try {
         // Commencez la vente (par exemple, suppression de la transaction)
-        const index = this.userTransactions.findIndex((t) => t.id === transaction.id);
+        const index = this.userTransactions.findIndex(
+          (t) => t.id === transaction.id
+        );
         if (index !== -1) {
           this.userTransactions.splice(index, 1);
         }
 
         // Mettez à jour le solde fictif du compte
-        if (!isNaN(this.fakeAccountBalance) && !isNaN(transaction.selling_price)) {
+        if (
+          !isNaN(this.fakeAccountBalance) &&
+          !isNaN(transaction.selling_price)
+        ) {
           this.fakeAccountBalance += parseFloat(transaction.selling_price);
         } else {
           console.log("Invalid value for fakeAccountBalance or selling_price.");
@@ -177,30 +212,42 @@ export default {
     },
 
     isNegativeCapitalGain(transaction) {
-      if (!isNaN(transaction.purchase_price) && !isNaN(transaction.selling_price)) {
+      if (
+        !isNaN(transaction.purchase_price) &&
+        !isNaN(transaction.selling_price)
+      ) {
         const purchasePrice = parseFloat(transaction.purchase_price);
         const sellingPrice = parseFloat(transaction.selling_price);
-        const capitalGain = (sellingPrice - purchasePrice) * transaction.quantity;
+        const capitalGain =
+          (sellingPrice - purchasePrice) * transaction.quantity;
         return capitalGain < 0;
       }
       return false;
     },
 
     isPositiveCapitalGain(transaction) {
-      if (!isNaN(transaction.purchase_price) && !isNaN(transaction.selling_price)) {
+      if (
+        !isNaN(transaction.purchase_price) &&
+        !isNaN(transaction.selling_price)
+      ) {
         const purchasePrice = parseFloat(transaction.purchase_price);
         const sellingPrice = parseFloat(transaction.selling_price);
-        const capitalGain = (sellingPrice - purchasePrice) * transaction.quantity;
+        const capitalGain =
+          (sellingPrice - purchasePrice) * transaction.quantity;
         return capitalGain > 0;
       }
       return false;
     },
 
     formattedCapitalGain(transaction) {
-      if (!isNaN(transaction.purchase_price) && !isNaN(transaction.selling_price)) {
+      if (
+        !isNaN(transaction.purchase_price) &&
+        !isNaN(transaction.selling_price)
+      ) {
         const purchasePrice = parseFloat(transaction.purchase_price);
         const sellingPrice = parseFloat(transaction.selling_price);
-        const capitalGain = (sellingPrice - purchasePrice) * transaction.quantity;
+        const capitalGain =
+          (sellingPrice - purchasePrice) * transaction.quantity;
         return new Intl.NumberFormat("fr-FR", {
           style: "currency",
           currency: "EUR",
